b386462acf1233e29b306ef13271a6c247b5c62d
Michael Dietrich <dietrich@zeiss-izm.de>
2020-01-20 15:42:44 +0100

    chore: Create simple WPF application sample

---
+++ LocalizationTest/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Resources;
+using System.Windows;
+
+namespace LocalizationTest
+{
+    public partial class App
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var resourceManager = new ResourceManager(GetType());
+
+            var window = new Window
+            {
+                Title = resourceManager.GetString("ResourceKey", CultureInfo.GetCultureInfo("de"))
+            };
+
+            window.Show();
+        }
+    }
+}
